9691a500b6afca7bdb8bfe666ac20b9743d567ef
skdltm517 <54790785+skdltm517@users.noreply.github.com>
2021-03-30 15:11:26 +0900

    Add files via upload

---
+++ 14_preprocessor/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linkedList.c"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	bubble_sort();
+	return 0;
+}
